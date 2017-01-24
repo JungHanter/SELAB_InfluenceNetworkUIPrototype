ef188793da5ee91607e8a33dc241c9d3d0bf4427
SM <hcooch2ch3@gmail.com>
2017-01-24 18:18:59 +0900

    sm implementating layout of main

---
+++ js/main.js
@@ -0,0 +1,37 @@
+
+$('#create').click(function () {
+
+});
+
+$('#save').click(function () {
+
+});
+
+$('#save-as').click(function () {
+
+});
+
+$('#delete').click(function () {
+
+});
+
+$('#print').click(function () {
+
+});
+
+$('#edit-nodes').click(function () {
+
+});
+
+$('#edit-influences').click(function () {
+
+});
+$('#edit-confidences').click(function () {
+
+});
+$('#max-influence-node').click(function () {
+
+});
+$('#max-influence-group').click(function () {
+
+});
